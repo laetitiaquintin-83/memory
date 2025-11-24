aece569b5fd0eb6f059db592878dc1ea2bf7079f
laetitiaquintin-83 <laetitia.quintin@laplateforme.io>
2025-11-24 15:48:53 +0100

    Ajout de la classe User pour gérer la création et la récupération des utilisateurs dans la base de données.

---
+++ app/Models/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Core\Database;
+
+class user
+{
+
+    public function create($login, $password, $email, $nom, $prenom)
+    {
+        $pdo = Database::getPdo();
+
+        $sql = "INSERT INTO utilisateurs (login, mot-de-passe, email, nom, prenom, date_creation)
+        VALUES (?, ?, ?, ?, ?, NOW())";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$login, $password, $email, $nom, $prenom ]);
+
+        return $stmt;
+
+    }
+ 
+    public function findByLogin($login)
+    {
+        $pdo = Database::getPdo();
+
+        $sql = "SELECT * FROM utilisateurs WHERE login = ?";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([$login]);
+
+        return $stmt->fetch(\PDO::FETCH_ASSOC);
+    }
+
+    public function exists($login)
+    {
+        $user = $this->findByLogin($login);
+        return $user !== false;
+    }
+}
