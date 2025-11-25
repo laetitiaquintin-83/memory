929d3b230e05f7aa493a9776517c5c5d0af2a97f
laetitiaquintin-83 <laetitia.quintin@laplateforme.io>
2025-11-25 15:42:45 +0100

    Amélioration de l'affichage du plateau de jeu avec ajout d'un chronomètre et d'une barre d'infos stylisée

--- app/Views/game/plateau.php
+++ app/Views/game/plateau.php
@@ -1,25 +1,46 @@
 <?php
+$maintenant = time();
+$debut = $_SESSION['debut_partie'] ?? $maintenant;
+$tempsEcoule = $maintenant - $debut;
 
-for ($i = 0;  $i < count($jeu); $i++) {
-
-    $carte = $jeu[$i];
-    ?>
-
-    <div class="carte-conteneur">
-
-    <?php if ($carte->getEstRetournee()): ?>
-        <img src="<?= $carte->getImage() ?>" alt ="Image du memory">
-
-        <?php else: ?>
-            <a href="/game/play?i=<?= $i ?>">
-                <div class="dos"></div>
-        </a>
-        <?php endif; ?>
-
-        </div>
-
-        <?php
-       
-}
+$chronoAffiche = gmdate("i:s", $tempsEcoule);
 ?>
+
+<div class="game-container">
+
+<div class="info-bar">
+    <a href="/game" class="btn-abandon">Abandonner</a>
+
+    <div class="timer-box">
+       <span>Temps :</span>
+       <span style="font-family: monospace; font-size: 1.2em;">
+        <?= $chronoAffiche ?>
+</span>
 </div>
+</div>
+
+<div id="plateau-jeu">
+    <?php
+    for ($i = 0; $i < count($jeu); $i++){
+        $carte = $jeu[$i];
+        $classeSpeciale = $carte->getEstTrouvee() ? 'trouvee' : "";
+        ?>
+
+        <div class="carte-conteneur <?= $classeSpeciale ?>
+        <?php if ($carte->getEstRetournee() || $carte->getEstTrouvee()): ?>
+                    <img src="<?= $carte->getImage() ?>" alt="Carte Memory" class="carte-img">
+                
+                <?php else: ?>
+                    <a href="/game/play?i=<?= $i ?>" style="display:block; width:100%; height:100%; text-decoration:none;">
+                        <div class="dos"></div>
+                    </a>
+                <?php endif; ?>
+
+            </div>
+
+        <?php 
+        } // Fin de la boucle for
+        ?>
+    </div>
+    
+        
