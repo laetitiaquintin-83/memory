7002b1c9e10b4f868be0d0a252122aa01e12a204
laetitiaquintin-83 <laetitia.quintin@laplateforme.io>
2025-11-25 11:50:12 +0100

    Ajout de la méthode de mise à jour des informations utilisateur avec gestion sécurisée du mot de passe et récupération de l'ID depuis la session.

--- app/Models/User.php
+++ app/Models/User.php
@@ -38,4 +38,47 @@ class user
         $user = $this->findByLogin($login);
         return $user !== false;
     }
-}
+    /**
+     * Met à jour les infos de l'utilisateur
+     */
+
+    public function update($login, $email, $nom, $prenom, $password = null)
+    {
+        //Sécurité; On récupère l'Id depuis la session
+        if (!isset($_SESSION['user']['id'])){
+            return false;
+        }
+        $id= $_SESSION['user']['id'];
+        $pdo = Database::getPdo();
+
+        //Si le mot de passe est fourni; on met tout à jour
+        if ($password){
+            $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "UDPATE utilisateurs
+            SET login = ?, email = ?, nom = ?, prenom = ?, password = ?
+            WHERE id = ?";
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$login, $email, $nom, $prenom, $hashedPassword, $id]);
+
+        }else{
+            //sinon, on ne touche pas au mot de passe
+            $sql = "UPDATE utilisateurs
+            SET login = ?, email = ?, nom = ?, prenom = ?
+            WHERE id = ?";
+
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$login, $email, $nom, $prenom, $id]);
+        }
+
+        return true;
+    }
+        public function findById($id)
+        {
+            $pdo = Database::getPdo();
+            $stmt = $pdo->prepare("SELECT * FROM utilisateurs WHERE id = ?");
+            $stmt->execute([$id]);
+            return $stmt->fetch();
+        }
+    }
